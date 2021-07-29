9bc459de1033762bf50f8d7abbe313ba4f1813fc
P-Chan <hello@0x50.io>
2021-07-29 14:27:44 +0900

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = [
+  'last 2 chrome versions',
+  'last 2 firefox versions',
+  'last 2 edge versions',
+  'last 2 safari versions',
+  'last 1 and_chr version',
+  'last 2 ios_saf versions',
+]
